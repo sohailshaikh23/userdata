d2e374c07ef4be04ceb28b453d011c56b7f229e7
sohailshaikh23 <136561120+sohailshaikh23@users.noreply.github.com>
2024-02-26 14:27:30 +0530

    Update tomcat9_ubuntu.sh

--- tomcat9_ubuntu.sh
+++ tomcat9_ubuntu.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # This userdata for tomcat9 is for UBUNTU
-sudo apt update
+sudo apt update -y
 sudo apt upgrade -y
 sudo apt install openjdk-11-jdk -y
 sudo apt install tomcat9 tomcat9-admin tomcat9-docs tomcat9-common git -y
