2c817a3386896c18b24c0ab4defd2b8a7e9f3dee
sohailshaikh23 <136561120+sohailshaikh23@users.noreply.github.com>
2024-02-26 08:02:00 +0530

    Update tomcat9_ubuntu.sh

--- tomcat9_ubuntu.sh
+++ tomcat9_ubuntu.sh
@@ -1,1 +1,9 @@
+#!/bin/bash
 
+# This userdata for tomcat9 is for UBUNTU
+sudo apt update
+sudo apt upgrade -y
+sudo apt install openjdk-11-jdk -y
+sudo apt install tomcat9 tomcat9-admin tomcat9-docs tomcat9-common git -y
+
+#Default port is 8080
