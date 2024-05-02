bd63b838b50d80a222c51f26481aee35cdefa357
sohailshaikh23 <136561120+sohailshaikh23@users.noreply.github.com>
2024-05-02 12:55:06 +0530

    Update docker.sh

--- docker.sh
+++ docker.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 sudo apt update -y
-sudo apt install docker.io -y
-sudo apt install docker-compose -y
+sudo apt install docker.io -y  # v24.0.7
+sudo apt install docker-compose -y # v1.29
 sudo chmod 777 /var/run/docker.sock
