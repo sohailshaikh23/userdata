47adc1ac8a79db6de1cd99c20c40c853dc082acc
sohailshaikh23 <136561120+sohailshaikh23@users.noreply.github.com>
2024-05-03 12:07:25 +0530

    Update docker.sh

--- docker.sh
+++ docker.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 sudo apt update -y
 sudo apt install docker.io -y  # v24.0.7
-sudo apt install docker-compose -y # v1.29
 sudo chmod 777 /var/run/docker.sock
+
+mkdir -p ~/.docker/cli-plugins/
+curl -SL https://github.com/docker/compose/releases/download/v2.3.3/docker-compose-linux-x86_64 -o ~/.docker/cli-plugins/docker-compose
+
+chmod +x ~/.docker/cli-plugins/docker-compose
